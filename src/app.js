5036ad6449cd8810fbc05812984828922205d138
Okan Dodanli <djokanaslan@gmail.com>
2015-11-28 02:35:51 -0500

    Adding simple modification for design

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ var ajax = require('ajax');
 Accel.init();
 
 var main = new UI.Card({
-  title: 'Pebble.js',
+  title: 'Virtual Pebble',
   icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
+  subtitle: 'Welcome',
   body: 'Press any button.'
 });
 
