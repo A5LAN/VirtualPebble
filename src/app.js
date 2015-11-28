00bc532423e6948eda1d05d62687518ed88f2496
Hannes Filler <hfiller2@uwo.ca>
2015-11-28 03:17:59 -0500

    adding ajax request

--- src/app.js
+++ src/app.js
@@ -10,21 +10,33 @@ var Accel = require('ui/accel');
 var ajax = require('ajax');
 Accel.init();
 var main = new UI.Card({
-  title: 'Virtual Pebble',
-  icon: 'images/item_icon.png',
-  subtitle: 'Welcome',
-  body: 'Press any button.'
+	title: 'Virtual Pebble',
+	icon: 'images/item_icon.png',
+	subtitle: 'Welcome',
+	body: 'Press any button.'
 });
 
 main.show();
-
-main.on('click', 'up', function(e) {
-  Accel.on('data', function(e) {
-    for( var accelIndex in e.accels ){
-      console.log("x:"+e.accels[accelIndex].x);
-      console.log("y:"+e.accels[accelIndex].y);
-      console.log("z:"+e.accels[accelIndex].z);
-      console.log("time:"+e.accels[accelIndex].time);
-    }
-  });
-});
+function dataHandler(data){
+	var accelSum = {x:data.accel.x,y:data.accel.y,z:data.accel.z};
+	var lastAccel = data.accel;
+	for (var i = 1; i < data.samples; i++) {
+		accelSum.x += data.accels[i].x;
+		accelSum.y += data.accels[i].y;
+		accelSum.z += data.accels[i].z;
+	};
+	ajax(
+		{
+			url: 'http://ianisabitch.xyz/balls',
+			type: 'json',
+			data:accelSum
+		},
+		function(data, status, request) {
+			console.log('Quote of the day is: ' + data);
+		},
+		function(error, status, request) {
+			console.log('The ajax request failed: ' + error);
+		}
+	);
+}
+Accel.on("data")
