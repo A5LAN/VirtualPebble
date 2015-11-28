835fd33d4f8c46b2890badd6cf573056408bc497
Okan Dodanli <djokanaslan@gmail.com>
2015-11-28 03:57:43 -0500

    FIXED THE DAMN ICON

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ var ajax = require('ajax');
 Accel.init();
 var main = new UI.Card({
 	title: 'Virtual Pebble',
-	icon: 'resources/images/lion.png',
+	icon: 'images/menu_icon.png',
 	subtitle: 'Welcome',
 	body: 'Press any button.'
 });
