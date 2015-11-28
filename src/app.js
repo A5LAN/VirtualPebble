6aee89bca40fc4ca68f775b1d33fe8c17af28a84
Hannes Filler <hfiller2@uwo.ca>
2015-11-28 03:44:37 -0500

    added jerk calculation

--- src/app.js
+++ src/app.js
@@ -19,17 +19,22 @@ var main = new UI.Card({
 main.show();
 function dataHandler(data){
 	var accelSum = {x:data.accel.x,y:data.accel.y,z:data.accel.z};
+	var jerkSum = {x:0,y:0,z:0}
 	var lastAccel = data.accel;
 	for (var i = 1; i < data.samples; i++) {
 		accelSum.x += data.accels[i].x;
 		accelSum.y += data.accels[i].y;
 		accelSum.z += data.accels[i].z;
+		jerkSum.x += lastAccel.x - data.accels[i].x;
+		jerkSum.y += lastAccel.y - data.accels[i].y;
+		jerkSum.z += lastAccel.z - data.accels[i].z;
+		lastAccel = data.accels[i];
 	};
 	ajax(
 		{
 			url: 'http://ianisabitch.xyz/balls',
 			type: 'json',
-			data:accelSum
+			data: {accel:accelSum, jerk:jerkSum}
 		},
 		function(data, status, request) {
 			console.log('Quote of the day is: ' + data);
