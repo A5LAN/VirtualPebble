014e2a5411ac4a4594c2bb70c3ceb98c1259fabc
Okan Dodanli <djokanaslan@gmail.com>
2015-11-28 02:55:01 -0500

    icon change

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ var ajax = require('ajax');
 Accel.init();
 var main = new UI.Card({
   title: 'Virtual Pebble',
-  icon: 'images/menu_icon.png',
+  icon: 'images/lion.png',
   subtitle: 'Welcome',
   body: 'Press any button.'
 });
@@ -20,11 +20,8 @@ main.show();
 
 main.on('click', 'up', function(e) {
   Accel.on('data', function(e) {
-    for( var accelIndex in e.accels ){
-      console.log("x:"+e.accels[accelIndex].x);
-      console.log("y:"+e.accels[accelIndex].y);
-      console.log("z:"+e.accels[accelIndex].z);
-      console.log("time:"+e.accels[accelIndex].time);
+    for( var test in e ){
+      console.log(e[test]);
     }
   });
 });
